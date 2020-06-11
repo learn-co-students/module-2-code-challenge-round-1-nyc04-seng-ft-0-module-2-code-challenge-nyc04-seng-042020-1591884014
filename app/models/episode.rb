e22db61d8c55ba9c4bb66030e648a7ef033572b4
bashir2017 <bashiralhanshali@yahoo.com>
2020-06-11 11:46:02 -0400

    mod2 challange solution

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,2 +1,9 @@
 class Episode < ApplicationRecord
+    has_many :appearances 
+    has_many :guests, through: :appearances
+
+    #helper method to find average rating
+    def average_rating 
+        self.appearances.average(:rating)
+    end 
 end
--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,3 +1,16 @@
 class Guest < ApplicationRecord
+    has_many :appearances 
+    has_many :episodes, through: :appearances
 
+
+    #helper method to find the guest's appearance rating of a particular episode
+    def appearance_rating(episode)
+        appearance = Appearance.find_by({episode_id: episode.id, guest_id: self.id})
+        appearance.rating 
+    end
+
+    #returns an array of guest's episodes, sorted from highest to lowest rating
+    def sorted_episodes
+        self.episodes.sort_by{|episode| -self.appearance_rating(episode)}
+    end 
 end
