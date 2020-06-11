5426924b17f1238e93b8b98470e7ac1cc86b8279
github username <github email address>
2020-06-11 11:39:43 -0400

    completed bonuses

--- app/models/appearance.rb
+++ app/models/appearance.rb
@@ -3,4 +3,5 @@ class Appearance < ApplicationRecord
   belongs_to :episode
 
   validates :rating, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 5, message: "must be between 1 and 5" }
+  validates :guest_id, uniqueness: {scope: :episode_id, message: "has already been taken for this episode"}
 end
