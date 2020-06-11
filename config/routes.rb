3ef9e041eb2aa9e53b110e1c1d30ad919b23068b
iamkevn <iamkevnrodriguez@gmail.com>
2020-06-11 12:31:46 -0400

    incompelete

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+
+  get 'appearances/new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :guests, only: [:index]
   resources :episodes, only: [:index]
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -25,3 +25,6 @@ date = Date.parse('2015-09-08')
   date = date.next
 end
 
+appearances = Appearance.create(rating: 1, guest_id: 1, episode_id: 1)
+
+
